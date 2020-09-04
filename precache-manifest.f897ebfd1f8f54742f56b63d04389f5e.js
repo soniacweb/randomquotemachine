5abd16aa75360fe1769855522c25b5513981296a
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-09-04 16:04:37 +0100

    Updates

---
+++ precache-manifest.f897ebfd1f8f54742f56b63d04389f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7eb72230d1740fafb0dda91084120d",
+    "url": "/randomquotemachine/index.html"
+  },
+  {
+    "revision": "bbfc224b5a3df2371b6d",
+    "url": "/randomquotemachine/static/css/main.d1226872.chunk.css"
+  },
+  {
+    "revision": "05a24c1cc457b7762da6",
+    "url": "/randomquotemachine/static/js/2.d3769f4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomquotemachine/static/js/2.d3769f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfc224b5a3df2371b6d",
+    "url": "/randomquotemachine/static/js/main.6292bc6d.chunk.js"
+  },
+  {
+    "revision": "634ab8e01a304aa3c7ed",
+    "url": "/randomquotemachine/static/js/runtime-main.ce0d4038.js"
+  }
+]);
